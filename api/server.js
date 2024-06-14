213d115d6a3a368343fbfb91d0cb6a6b806c1a4d
thancha.tip <thanchatip.coe@gmail.com>
2024-06-14 15:25:45 +0700

    Improve error handling and logging

--- api/server.js
+++ api/server.js
@@ -1,39 +1,28 @@
+// api/server.js
 const express = require("express");
 const cors = require("cors");
 const xlsx = require("xlsx");
-const app = express();
 const serverless = require("serverless-http");
-const port = process.env.PORT || 3001;
+const path = require("path");
 
+const app = express();
 app.use(cors());
 
 app.get("/api/population", (req, res) => {
   try {
-    console.log("req...", req.query.year);
     const year = req.query.year;
-    console.log("Reading Excel file...");
-    const workbook = xlsx.readFile("population-and-demography.csv");
-    const sheet_name_list = workbook.SheetNames;
-    const sheet = workbook.Sheets[sheet_name_list[0]];
-
-    console.log("Converting sheet to JSON...");
-    const jsonData = xlsx.utils.sheet_to_json(sheet, {
-      header: 1,
-      raw: false,
-    });
-
-    console.log("Removing header row...");
+    const workbook = xlsx.readFile(
+      path.resolve(__dirname, "population-and-demography.csv")
+    );
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = xlsx.utils.sheet_to_json(sheet, { header: 1, raw: false });
     const dataWithoutHeader = jsonData.slice(1);
-
     const formattedData = dataWithoutHeader.map((row) => ({
       country: row[0],
       year: row[1],
       population: row[2],
     }));
-
     const filteredData = formattedData.filter((entry) => entry.year == year);
-
-    console.log("filteredData Data...", filteredData);
     let uniqueCountries = {};
     filteredData.forEach((entry) => {
       let country = entry["country"];
@@ -41,69 +30,49 @@ app.get("/api/population", (req, res) => {
         uniqueCountries[country] = entry;
       }
     });
-
-    console.log("uniqueCountries", uniqueCountries);
     let groupedCountries = {};
     Object.values(uniqueCountries).forEach((countryObj) => {
       let firstLetter = countryObj["country"][0].toUpperCase();
-
       if (!groupedCountries[firstLetter]) {
         groupedCountries[firstLetter] = [];
       }
       groupedCountries[firstLetter].push(countryObj);
     });
-
     let selectedCountriesArray = [];
     for (let letter in groupedCountries) {
       selectedCountriesArray.push(groupedCountries[letter][0]);
     }
-
-    console.log(selectedCountriesArray);
     res.json(selectedCountriesArray);
   } catch (error) {
-    console.error("Error processing request:", error);
+    console.error("Error processing /api/population request:", error);
     res.status(500).send("Error processing request");
   }
 });
 
 app.get("/api/year-filters", (req, res) => {
   try {
-    console.log("Reading Excel file...");
-    const workbook = xlsx.readFile("population-and-demography.csv");
-
-    const sheet_name_list = workbook.SheetNames;
-    const sheet = workbook.Sheets[sheet_name_list[0]];
-
-    console.log("Converting sheet to JSON...");
-    const jsonData = xlsx.utils.sheet_to_json(sheet, {
-      header: 1,
-      raw: false,
-    });
-
-    console.log("Removing header row...");
+    const workbook = xlsx.readFile(
+      path.resolve(__dirname, "population-and-demography.csv")
+    );
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = xlsx.utils.sheet_to_json(sheet, { header: 1, raw: false });
     const dataWithoutHeader = jsonData.slice(1);
-
     const formattedData = dataWithoutHeader.map((row) => ({
       country: row[0],
       year: row[1],
       population: row[2],
     }));
-
-    console.log("filteredData Data...", formattedData);
-
     let uniqueYears = [];
     formattedData.forEach((entry) => {
       let year = entry["year"];
-      if (!uniqueYears[year]) {
-        uniqueYears.push(entry.year);
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
       }
     });
-
-    console.log("uniqueYears", uniqueYears);
     const uniqueYearOptions = [...new Set(uniqueYears.map(Number))];
     res.json(uniqueYearOptions);
   } catch (error) {
-    console.error("Error processing request:", error);
+    console.error("Error processing /api/year-filters request:", error);
     res.status(500).send("Error processing request");
   }
 });
